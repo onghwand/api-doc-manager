531e0f7f51dfd18689ddfea458d7f1c2ddff949c
이동환/광고플랫폼개발팀 <ldh2364@lguplus.co.kr>
2025-11-27 10:32:28 +0900

    fix api

--- src/api_doc_manager/simple_api.py
+++ src/api_doc_manager/simple_api.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello")
-def read_root(param: str):
+@app.get("/hello1234")
+def read_root(param: str, param2: str):
     """
     Simple API that accepts one parameter and returns 'hello'.
     """
-    return "hello123"
+    return "hello"
