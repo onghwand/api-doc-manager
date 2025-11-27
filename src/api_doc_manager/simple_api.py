bbd5bbee574c4db6c490940639d6f2315ab31d43
이동환/광고플랫폼개발팀 <ldh2364@lguplus.co.kr>
2025-11-27 10:32:58 +0900

    fix api

--- src/api_doc_manager/simple_api.py
+++ src/api_doc_manager/simple_api.py
@@ -8,3 +8,10 @@ def read_root(param: str, param2: str):
     Simple API that accepts one parameter and returns 'hello'.
     """
     return "hello"
+
+@app.get("/test")
+def read_root(param3: str, param4: str):
+    """
+    Simple API that accepts one parameter and returns 'hello'.
+    """
+    return "test1"
